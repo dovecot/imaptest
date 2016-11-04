dovecot/imaptest

commit 13870004147e39accae9806eb332870bdf881816

Fixed literal+ handling.
 - CRs weren't handled right
 - If command already had literal+, it wasn't sent at all

src/commands.c
@@ -83,6 +83,9 @@ command_get_cmdline(struct imap_client *client, const char **_cmdline,
 			cmdline_len -= len;
 		} else {
 			/* using a literal */
+			if (p[-1] == '\r') {
+				p--;
+			}
 			if ((client->capabilities & CAP_LITERALPLUS) == 0 &&
 			    p[-2] == '+') {
 				/* using literal+ without server support,
@@ -95,6 +98,8 @@ command_get_cmdline(struct imap_client *client, const char **_cmdline,
 				str_append(str, "+}");
 			} else if (p[-2] != '+') {
 				i_fatal("FIXME: Add support for sync literals");
+			} else {
+				buffer_append(str, cmdline, p-cmdline);
 			}
 			str_append(str, "\r\n");
 			cmdline += len;

commit 897d9242d30ad62d36f52e6e5669f95ca4239fa3

Fully fix literal handling.
13870004147e39accae9806eb332870bdf881816 broke some tests.

src/commands.c
@@ -86,6 +86,7 @@ command_get_cmdline(struct imap_client *client, const char **_cmdline,
 			if (p[-1] == '\r') {
 				p--;
 			}
+			i_assert(p[-1] == '}');
 			if ((client->capabilities & CAP_LITERALPLUS) == 0 &&
 			    p[-2] == '+') {
 				/* using literal+ without server support,
@@ -94,7 +95,7 @@ command_get_cmdline(struct imap_client *client, const char **_cmdline,
 			} else if ((client->capabilities & CAP_LITERALPLUS) != 0 &&
 				   p[-2] != '+') {
 				/* for now we always convert to literal+ */
-				buffer_append(str, cmdline, p-cmdline-2);
+				buffer_append(str, cmdline, p-cmdline-1);
 				str_append(str, "+}");
 			} else if (p[-2] != '+') {
 				i_fatal("FIXME: Add support for sync literals");