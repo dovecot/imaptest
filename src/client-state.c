540fedb9218c123a2c7d873dd36edd7748336759
Stephan Bosch <stephan.bosch@dovecot.fi>
2017-01-02 02:05:46 +0100

    Updated copyright notices to match the normal Dovecot style and included the year 2017.

--- src/client-state.c
+++ src/client-state.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2007-2008 Timo Sirainen */
+/* Copyright (c) 2007-2017 ImapTest authors, see the included COPYING file */
 
 #include "lib.h"
 #include "base64.h"
--- src/user.c
+++ src/user.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013 Timo Sirainen */
+/* Copyright (c) 2013-2017 ImapTest authors, see the included COPYING file */
 
 #include "lib.h"
 #include "array.h"
