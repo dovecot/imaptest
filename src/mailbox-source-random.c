daf1f43e7d563e47b4b79685b3a16642ff4a3a1f
Stephan Bosch <stephan.bosch@dovecot.fi>
2018-01-01 22:42:00 +0100

    Updated copyright notices to include the year 2018.

--- src/mailbox-source-random.c
+++ src/mailbox-source-random.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2016-2017 ImapTest authors, see the included COPYING file */
+/* Copyright (c) 2016-2018 ImapTest authors, see the included COPYING file */
 
 #include "lib.h"
 #include "hash.h"
