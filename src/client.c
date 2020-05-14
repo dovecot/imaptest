ec797ee971cd98de7c5b4abf3c3d1c510f59e95d
Martti Rannanjärvi <martti.rannanjarvi@open-xchange.com>
2020-05-14 13:26:45 +0300

    client: Remove static maximum buffer size from input stream

--- src/client.c
+++ src/client.c
@@ -241,7 +241,7 @@ int client_init(struct client *client, unsigned int idx,
 
 	client->fd = fd;
 	client->rawlog_fd = -1;
-	client->input = i_stream_create_fd(fd, 1024*64);
+	client->input = i_stream_create_fd(fd, (size_t)-1);
 	client->output = o_stream_create_fd(fd, (size_t)-1);
 	i_stream_set_name(client->input, t_strdup_printf("client %u", idx));
 	o_stream_set_name(client->output, t_strdup_printf("client %u", idx));
