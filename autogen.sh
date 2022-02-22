eb2ce83ef6f6f5d0554d2a6cf1e95f18b15a5c9d
Aki Tuomi <aki.tuomi@open-xchange.com>
2022-02-22 10:17:49 +0200

    autogen.sh: Do not set ACLOCAL

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
 # If you've non-standard directories, set these
-#ACLOCAL_DIR=
-
-if test "$ACLOCAL_DIR" != ""; then
-  ACLOCAL="aclocal -I $ACLOCAL_DIR"
-  export ACLOCAL
-fi
 
 autoreconf -i
