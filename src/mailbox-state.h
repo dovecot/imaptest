91735af7715ef9525aefaba179a43a4ca7536f54
Markus Valentin <markus.valentin@open-xchange.com>
2025-04-22 12:02:25 +0200

    Introduce failures and errors if IMAP4REV2 is ignored

--- src/mailbox-state.h
+++ src/mailbox-state.h
@@ -7,8 +7,10 @@ void mailbox_state_handle_fetch(struct imap_client *client, unsigned int seq,
 				const struct imap_arg *args);
 
 int mailbox_state_set_flags(struct mailbox_view *view,
-			     const struct imap_arg *args);
+			    const struct imap_arg *args,
+			    bool imap4rev2_enabled);
 int mailbox_state_set_permanent_flags(struct mailbox_view *view,
-				      const struct imap_arg *args);
+				      const struct imap_arg *args,
+				      bool imap4rev2_enabled);
 
 #endif
