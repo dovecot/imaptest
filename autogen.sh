ac5d0b1c94ff5bcdff0ee2111d2714a4c7e0cee4
Aki Tuomi <aki.tuomi@open-xchange.com>
2025-11-14 10:24:45 +0200

    autogen: Remove gettext

--- autogen.sh
+++ autogen.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
 
-# If you've non-standard directories, set these
-#GETTEXT_DIR=
-
-for dir in $GETTEXT_DIR /usr/share/gettext; do
-  if test -f $dir/config.rpath; then
-    /bin/cp -f $dir/config.rpath build-aux/
-    break
-  fi
-done
-
 autoreconf -vif
